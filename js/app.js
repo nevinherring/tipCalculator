dbfb198a7ff89ca254738a0209136d7b98b34b78
Nevin Herring <nevin.herring@gmail.com>
2018-12-29 17:49:30 -0600

    finished html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//select elements
+let input = document.querySelector("input");
+let btnTen = document.getElementById("ten");
+let btnFifteen = document.getElementById("fifteen");
+let btnTwenty = document.getElementById("twenty");
+let tipTotal = document.getElementById("tipTotal");
+let billTotal = document.getElementById("billTotal");
+
+//create event listeners
+btnTen.addEventListener("click", function(){
+    console.log(input);
+});
+
+//write functions
